666f4b21f170f73040dced78fc20ef2a08070994
David Hemming <dhemming@odecee.com.au>
2017-02-24 16:48:36 +1100

    Init commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+
+var port = process.env.PORT || 3000;
+var app = express();
+
+console.log(publicPath);
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+	console.log(`Server is up on port ${port}`);
+});
