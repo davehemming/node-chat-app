b49c9ac093945ae53de355e36d8681b4007713e2
David Hemming <dhemming@odecee.com.au>
2017-02-27 11:02:56 +1100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
 	console.log('Connected to server');
-
-	socket.emit('createMessage', {
-		from: 'Blue',
-		text: 'Anyone want to blue with blue?'
-	})
 });
 
 socket.on('disconnect', function() {
